ff0cac572dd2ee1986159ff0d34e02ebb040efe0
ROHIT CHOUREY <97465195+rohitchourey0809@users.noreply.github.com>
2024-06-15 15:54:35 +0530

    all item aligned

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { ChakraProvider, Box, Heading, Center } from "@chakra-ui/react";
-import GroupForm from "./components/GroupForm";
-import GroupStatus from "./components/GroupStatus";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import GroupFormAndStatus from "./components/GroupFormAndStatus";
@@ -11,11 +9,9 @@ const App = () => {
     <ChakraProvider>
       <Provider store={store}>
         <Box p={4}>
-          {/* <Center>
-            <Heading mb={4}>Group Status Viewer</Heading>
+          <Center>
+            <Heading>Group Status</Heading>
           </Center>
-          <GroupForm /> */}
-          {/* <GroupStatus /> */}
           <GroupFormAndStatus />
         </Box>
       </Provider>
